8e1f732849c45e4599b680ea21a8032bb0e40cf9
Ian Grams <Omega192@gmail.com>
2014-02-25 20:13:48 -0500

    Added barebones Fragments

---
+++ CaseHub/src/com/example/casehub/LaundryFragment.java
@@ -0,0 +1,17 @@
+package com.example.casehub;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LaundryFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_laundry, container, false);
+    }
+}
+
