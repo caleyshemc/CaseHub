a5cb6005389d50d7002134f6be90721fa9fe01dd
Caley Shem-Crumrine <caley@case.edu>
2014-02-28 15:39:43 -0500

    Added schedule fragment.

--- CaseHub/src/com/example/casehub/ScheduleFragment.java
+++ CaseHub/src/com/example/casehub/ScheduleFragment.java
@@ -1,17 +1,27 @@
 package com.example.casehub;
 
 import android.os.Bundle;
-import android.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 
-public class ScheduleFragment extends Fragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+// TODO: rename to ScheduleLoginFragment and create separate SceduleFragment? Or make an Activity?
+public class ScheduleFragment extends DialogFragment {
+
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.login_prompt)
+               .setPositiveButton(R.string.login_button, new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       // FIRE ZE MISSILES!
+                   }
+               });
+        
+        // Create the AlertDialog object and return it
+        return builder.create();
     }
+
 }
 
