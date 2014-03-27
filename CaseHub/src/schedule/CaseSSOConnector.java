e433e74cda62d8e7d9f35b140b6c924e275fe60d
Caley Shem-Crumrine <caley@case.edu>
2014-03-26 20:04:28 -0400

    Successfully logged in to login.case.edu\!

--- CaseHub/src/schedule/CaseSSOConnector.java
+++ CaseHub/src/schedule/CaseSSOConnector.java
@@ -1,13 +1,37 @@
 package schedule;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
+import java.io.InputStreamReader;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookieStore;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import android.os.AsyncTask;
+import android.text.Html;
 import android.util.Log;
 
 /*
@@ -15,6 +39,10 @@ import android.util.Log;
  * TODO: Determine best I/O types for AsyncTask
  */
 public class CaseSSOConnector extends AsyncTask<String, Void, String> {
+
+	private String cookies;
+	private DefaultHttpClient client = new DefaultHttpClient();
+	private final String USER_AGENT = "Mozilla/5.0";
 
 	//private String destination;
 	
@@ -30,59 +58,58 @@ public class CaseSSOConnector extends AsyncTask<String, Void, String> {
 	 */
 	private String login(String user, String password) throws IOException {
 		
-		String result = "Login failed in login().";
+		String response = "Login failed in login().";
 
 		/*TODO TEST*/
-		user = "TEST";
-		password = "TEST";
+		user = "crs133";
+		password = "Fuckingpass1";
+		String url = "https://login.case.edu/cas/login";
 		
-		//URL url = new URL(SSO_URL + "https%3a%2f%2fm.case.edu%2fgadget_s.html%3f_gid%3dmyschedule");
-		URL url = new URL("https://login.case.edu/cas/login");
-		
-		// Specify POST request
-		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-		urlConnection.setDoOutput(true);
-		urlConnection.setRequestMethod("POST");
-		urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		BasicCookieStore cookieStore = new BasicCookieStore();
+	    client.setCookieStore(cookieStore);
 
-		// Specify POST parameters
-		String postParameters = "username="+user+"&password="+password;
-		//String postParameters = "userid="+user+"&pwd="+password;
-		urlConnection.setFixedLengthStreamingMode(postParameters.getBytes().length);
-		PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
-		out.print(postParameters);
-		out.close();
-		
-		java.util.Scanner s = null;
-		
-		try {
-			int statusCode = urlConnection.getResponseCode();
-			result = "Response code: " + Integer.toString(statusCode);
-		} catch (IOException e) {
-			e.printStackTrace();
-			InputStream in = new BufferedInputStream(urlConnection.getErrorStream());
-			s = new java.util.Scanner(in).useDelimiter("\\A");
-		    result = s.hasNext() ? s.next() : "";
-		    Log.d("RESULT",result);
-		}
-         
+	    
+	    /*
+		HttpPost post = new HttpPost(url);
 		
 		
-		try {
-			InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-			s = new java.util.Scanner(in).useDelimiter("\\A");
-		    result = s.hasNext() ? s.next() : "";
-		} catch (IOException e) {
-			e.printStackTrace();
-			InputStream in = new BufferedInputStream(urlConnection.getErrorStream());
-			s = new java.util.Scanner(in).useDelimiter("\\A");
-			result = s.hasNext() ? s.next() : "";
-			Log.d("RESULT",result);
-		} finally {
-			urlConnection.disconnect();
-		}
-		
-		return result;
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new BasicNameValuePair("username", user));
+			params.add(new BasicNameValuePair("password", password));
+		*/
+			
+			
+
+		HttpGet httpGet = new HttpGet(url);
+		HttpResponse result = client.execute(httpGet);
+
+		HttpEntity entity = result.getEntity();
+		String responseString = EntityUtils.toString(entity, "UTF-8");
+
+		Document doc = Jsoup.parse(responseString);
+		Elements input = doc.getElementsByAttributeValue("name", "lt");
+		String login_ticket = input.attr("value");
+
+		HttpPost httpPost = new HttpPost(url);
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		nameValuePairs.add(new BasicNameValuePair("lt", login_ticket));
+		nameValuePairs.add(new BasicNameValuePair("username", user));
+		nameValuePairs.add(new BasicNameValuePair("password", password));
+		httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+		result = client.execute(httpPost);
+
+		entity = result.getEntity();
+		response = EntityUtils.toString(entity, "UTF-8");
+
+		/*
+		httpGet = new HttpGet("https://m.case.edu/gadget_s.html?_gid=myschedule");
+		params.setParameter(ClientPNames.COOKIE_POLICY, "easy");
+		client.execute(httpGet, handler);
+		*/
+
+		    
+		return response;
 		
 	}
 
